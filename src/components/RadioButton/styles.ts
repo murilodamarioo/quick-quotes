29e0999f6a8bfac69d2c22b447a63dcf3243b158
Murilo Damario <murilo.gcdamario@gmail.com>
2026-02-15 17:47:55 -0300

    :sparkles: feat: Implements RadioButton component

---
+++ src/components/RadioButton/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    minWidth: 153,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  }
+})
