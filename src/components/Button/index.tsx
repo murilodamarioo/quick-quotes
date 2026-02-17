7ca003b8a7959ce0672647d507021e5eaa891ba1
Murilo Damario <murilo.gcdamario@gmail.com>
2026-02-17 15:09:04 -0300

    :recycle: refactor: Make style button flexible

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { Text, TextStyle, TouchableOpacity, TouchableOpacityProps, ViewStyle } from 'react-native'
 
 import { MaterialIcons } from '@expo/vector-icons'
 
@@ -7,13 +7,15 @@ import { styles } from './styles'
 type Props = TouchableOpacityProps & {
   name: keyof typeof MaterialIcons.glyphMap
   title: string
+  viewStyle?: ViewStyle
+  textStyle?: TextStyle
 }
 
-export function Button({ name, title, ...rest }: Props) {
+export function Button({ name, title, viewStyle, textStyle, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
-      <MaterialIcons name={name} size={24} style={styles.icon}/>
-      <Text style={styles.title}>{title}</Text>
+    <TouchableOpacity style={[styles.container, viewStyle]} {...rest}>
+      <MaterialIcons name={name} size={24} style={textStyle} />
+      <Text style={[styles.title, textStyle]}>{title}</Text>
     </TouchableOpacity>
   )
 }
