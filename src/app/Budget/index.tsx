d929ceca3542f3affe9ba2c8fd5c4d7dbb67d12b
Murilo Damario <murilo.gcdamario@gmail.com>
2026-02-17 15:20:19 -0300

    :sparkles: feat: Apply Scroll view

--- src/app/Budget/index.tsx
+++ src/app/Budget/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { View } from 'react-native'
+import { View, ScrollView } from 'react-native'
 
 import { styles } from './styles'
 
@@ -20,58 +20,63 @@ export function Budget({ navigation }: StackRoutesProps<'new_budget'>) {
   const [tagType, setTagType] = useState(TagStatus.DRAW)
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <PageHeader
-          title="Orçamento" tagStatus={TagStatus.SENT}
-          onPress={() => navigation.goBack()}
-        />
-      </View>
-
-      <View style={styles.content}>
-        <BudgetContent icon="home" title="Informacões gerais">
-          <View style={styles.form}>
-            <Input placeholder="Título" />
-            <Input placeholder="Cliente" />
-          </View>
-        </BudgetContent>
-
-        <BudgetContent icon="tag" title="Status">
-          <TagOption
-            selected={tagType}
-            onChange={setTagType}
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <PageHeader
+            title="Orçamento" tagStatus={TagStatus.SENT}
+            onPress={() => navigation.goBack()}
           />
-        </BudgetContent>
+        </View>
 
-        <BudgetContent style={{ paddingHorizontal: 20 }} icon='file-text' title='Serviços inclusos'>
-          <View style={styles.items}>
-            <Item
-              title='Design de interfaces'
-              description='Criação de wireframes e protótipos de alta fidelidade'
-              quantity={1}
-              price={100}
-            />
-            <Item
-              title='Design de interfaces'
-              description='Criação de wireframes e protótipos de alta fidelidade'
-              quantity={1}
-              price={100}
-            />
+        <View style={styles.content}>
+          <BudgetContent icon="home" title="Informacões gerais">
+            <View style={styles.form}>
+              <Input placeholder="Título" />
+              <Input placeholder="Cliente" />
+            </View>
+          </BudgetContent>
 
-            <Button
-              name='add'
-              title='Adicionar serviço'
-              onPress={() => console.log('Adicionar serviço')}
-              viewStyle={{
-                backgroundColor: colors.GRAY_100,
-                borderWidth: 1,
-                borderColor: colors.GRAY_300
-              }}
-              textStyle={{ color: colors.PURPULE_BASE }}
+          <BudgetContent icon="tag" title="Status">
+            <TagOption
+              selected={tagType}
+              onChange={setTagType}
             />
-          </View>
-        </BudgetContent>
+          </BudgetContent>
+
+          <BudgetContent style={{ paddingHorizontal: 20 }} icon='file-text' title='Serviços inclusos'>
+            <View style={styles.items}>
+              <Item
+                title='Design de interfaces'
+                description='Criação de wireframes e protótipos de alta fidelidade'
+                quantity={1}
+                price={100}
+              />
+              <Item
+                title='Design de interfaces'
+                description='Criação de wireframes e protótipos de alta fidelidade'
+                quantity={1}
+                price={100}
+              />
+
+              <Button
+                name='add'
+                title='Adicionar serviço'
+                onPress={() => console.log('Adicionar serviço')}
+                viewStyle={{
+                  backgroundColor: colors.GRAY_100,
+                  borderWidth: 1,
+                  borderColor: colors.GRAY_300
+                }}
+                textStyle={{ color: colors.PURPULE_BASE }}
+              />
+            </View>
+          </BudgetContent>
+
+          <BudgetContent icon='credit-card' title='Investimento'>
+          </BudgetContent>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
